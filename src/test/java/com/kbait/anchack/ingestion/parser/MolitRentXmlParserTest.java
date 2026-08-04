268588c7c222b6de61cbab4df081449141d32abe
swhyeon98 <smreo75@gmail.com>
2026-08-05 00:02:03 +0900

    test: 국토부 전월세 파서 예외 처리 검증

--- src/test/java/com/kbait/anchack/ingestion/parser/MolitRentXmlParserTest.java
+++ src/test/java/com/kbait/anchack/ingestion/parser/MolitRentXmlParserTest.java
@@ -3,10 +3,14 @@ package com.kbait.anchack.ingestion.parser;
 import com.kbait.anchack.ingestion.client.MolitRentApiCategory;
 import com.kbait.anchack.ingestion.dto.external.MolitRentPage;
 import com.kbait.anchack.ingestion.dto.external.RawRentalTransaction;
+import com.kbait.anchack.ingestion.exception.MolitRentApiResponseException;
+import com.kbait.anchack.ingestion.exception.MolitRentParseException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +18,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 class MolitRentXmlParserTest {
 
@@ -129,6 +134,188 @@ class MolitRentXmlParserTest {
         assertThat(page.getItems()).isEmpty();
     }
 
+    @Test
+    void response_오류는_body보다_먼저_API_응답_예외로_처리한다() {
+        String xml = """
+                <response>
+                    <header>
+                        <resultCode>99</resultCode>
+                        <resultMsg>INVALID_REQUEST</resultMsg>
+                    </header>
+                </response>
+                """;
+
+        MolitRentApiResponseException exception = assertApiResponseException(xml);
+
+        assertThat(exception.getResultCode()).isEqualTo("99");
+        assertThat(exception.getResultMessage()).isEqualTo("INVALID_REQUEST");
+        assertThat(exception.getMessage()).contains("99", "INVALID_REQUEST");
+    }
+
+    @Test
+    void 공공데이터포털_공통_오류는_API_응답_예외로_처리한다() {
+        String xml = """
+                <OpenAPI_ServiceResponse>
+                    <cmmMsgHeader>
+                        <errMsg>SERVICE_KEY_IS_NULL</errMsg>
+                        <returnAuthMsg>서비스 접근거부</returnAuthMsg>
+                        <returnReasonCode>20</returnReasonCode>
+                    </cmmMsgHeader>
+                </OpenAPI_ServiceResponse>
+                """;
+
+        MolitRentApiResponseException exception = assertApiResponseException(xml);
+
+        assertThat(exception.getResultCode()).isEqualTo("20");
+        assertThat(exception.getResultMessage()).isEqualTo("서비스 접근거부");
+    }
+
+    @Test
+    void 공통_오류에_returnAuthMsg가_없으면_errMsg를_사용한다() {
+        String xml = """
+                <OpenAPI_ServiceResponse>
+                    <cmmMsgHeader>
+                        <errMsg>SERVICE_KEY_IS_NULL</errMsg>
+                        <returnReasonCode>20</returnReasonCode>
+                    </cmmMsgHeader>
+                </OpenAPI_ServiceResponse>
+                """;
+
+        MolitRentApiResponseException exception = assertApiResponseException(xml);
+
+        assertThat(exception.getResultCode()).isEqualTo("20");
+        assertThat(exception.getResultMessage()).isEqualTo("SERVICE_KEY_IS_NULL");
+    }
+
+    @Test
+    void null_XML은_파싱_예외로_처리한다() {
+        MolitRentParseException exception = assertParseException(null);
+
+        assertThat(exception.getMessage()).contains("비어");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "   "})
+    void 빈_XML은_파싱_예외로_처리한다(String xml) {
+        MolitRentParseException exception = assertParseException(xml);
+
+        assertThat(exception.getMessage()).contains("비어");
+    }
+
+    @Test
+    void malformed_XML은_SAXException을_원인으로_보존한다() {
+        MolitRentParseException exception = assertParseException("<response><header></response>");
+
+        assertThat(exception).hasCauseInstanceOf(SAXException.class);
+    }
+
+    @Test
+    void DOCTYPE이_포함된_XML은_SAXException을_원인으로_보존한다() {
+        String xml = """
+                <!DOCTYPE response [<!ENTITY xxe SYSTEM "file:///etc/passwd">]>
+                <response>
+                    <header>
+                        <resultCode>000</resultCode>
+                        <resultMsg>OK</resultMsg>
+                    </header>
+                    <body>
+                        <items/>
+                        <pageNo>1</pageNo>
+                        <numOfRows>100</numOfRows>
+                        <totalCount>0</totalCount>
+                    </body>
+                </response>
+                """;
+
+        MolitRentParseException exception = assertParseException(xml);
+
+        assertThat(exception).hasCauseInstanceOf(SAXException.class);
+    }
+
+    @Test
+    void 지원하지_않는_최상위_요소는_파싱_예외로_처리한다() {
+        MolitRentParseException exception = assertParseException("<unsupported/>");
+
+        assertThat(exception.getMessage()).contains("최상위");
+    }
+
+    @Test
+    void header가_누락되면_파싱_예외로_처리한다() {
+        String xml = "<response>" + pageBody("1", "100", "0") + "</response>";
+
+        MolitRentParseException exception = assertParseException(xml);
+
+        assertThat(exception.getMessage()).contains("header");
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("invalidResultCodeCases")
+    void resultCode가_누락되거나_비어_있으면_파싱_예외로_처리한다(String caseName, String xml) {
+        MolitRentParseException exception = assertParseException(xml);
+
+        assertThat(exception.getMessage()).contains("resultCode");
+    }
+
+    @Test
+    void 정상_응답의_body가_누락되면_파싱_예외로_처리한다() {
+        MolitRentParseException exception = assertParseException(successResponse(""));
+
+        assertThat(exception.getMessage()).contains("body");
+    }
+
+    @Test
+    void 정상_응답의_items가_누락되면_파싱_예외로_처리한다() {
+        String body = "<body><pageNo>1</pageNo><numOfRows>100</numOfRows><totalCount>0</totalCount></body>";
+
+        MolitRentParseException exception = assertParseException(successResponse(body));
+
+        assertThat(exception.getMessage()).contains("items");
+    }
+
+    @ParameterizedTest(name = "{0} 누락")
+    @MethodSource("missingPageValueCases")
+    void 필수_페이지_요소가_누락되면_파싱_예외로_처리한다(String tagName, String xml) {
+        MolitRentParseException exception = assertParseException(xml);
+
+        assertThat(exception.getMessage()).contains(tagName);
+    }
+
+    @ParameterizedTest(name = "{0} 빈 값")
+    @MethodSource("blankPageValueCases")
+    void 페이지_값이_비어_있으면_파싱_예외로_처리한다(String tagName, String xml) {
+        MolitRentParseException exception = assertParseException(xml);
+
+        assertThat(exception.getMessage()).contains(tagName, "비어");
+    }
+
+    @ParameterizedTest(name = "{0} 비정수")
+    @MethodSource("invalidPageValueCases")
+    void 페이지_값이_정수가_아니면_숫자_형식_예외를_원인으로_보존한다(
+            String tagName,
+            String xml
+    ) {
+        MolitRentParseException exception = assertParseException(xml);
+
+        assertThat(exception.getMessage()).contains(tagName);
+        assertThat(exception).hasCauseInstanceOf(NumberFormatException.class);
+    }
+
+    private MolitRentApiResponseException assertApiResponseException(String xml) {
+        Throwable thrown = catchThrowable(() -> parser.parse(MolitRentApiCategory.OFFICETEL, xml));
+
+        assertThat(thrown)
+                .isExactlyInstanceOf(MolitRentApiResponseException.class)
+                .isNotInstanceOf(MolitRentParseException.class);
+        return (MolitRentApiResponseException) thrown;
+    }
+
+    private MolitRentParseException assertParseException(String xml) {
+        Throwable thrown = catchThrowable(() -> parser.parse(MolitRentApiCategory.OFFICETEL, xml));
+
+        assertThat(thrown).isExactlyInstanceOf(MolitRentParseException.class);
+        return (MolitRentParseException) thrown;
+    }
+
     private RawRentalTransaction parseFirstTransaction(
             String fixtureName,
             MolitRentApiCategory apiCategory
@@ -160,5 +347,57 @@ class MolitRentXmlParserTest {
                 Arguments.of("single_house_11620_202507.xml", MolitRentApiCategory.SINGLE_HOUSE, 1456),
                 Arguments.of("single_house_11620_202606.xml", MolitRentApiCategory.SINGLE_HOUSE, 1237)
         );
+    }
+
+    private static Stream<Arguments> invalidResultCodeCases() {
+        return Stream.of(
+                Arguments.of("resultCode 누락", "<response><header><resultMsg>OK</resultMsg></header></response>"),
+                Arguments.of(
+                        "resultCode 빈 값",
+                        "<response><header><resultCode> </resultCode><resultMsg>OK</resultMsg></header></response>"
+                )
+        );
+    }
+
+    private static Stream<Arguments> missingPageValueCases() {
+        return Stream.of(
+                Arguments.of("pageNo", successResponse(pageBody(null, "100", "0"))),
+                Arguments.of("numOfRows", successResponse(pageBody("1", null, "0"))),
+                Arguments.of("totalCount", successResponse(pageBody("1", "100", null)))
+        );
+    }
+
+    private static Stream<Arguments> blankPageValueCases() {
+        return Stream.of(
+                Arguments.of("pageNo", successResponse(pageBody("", "100", "0"))),
+                Arguments.of("numOfRows", successResponse(pageBody("1", "", "0"))),
+                Arguments.of("totalCount", successResponse(pageBody("1", "100", "")))
+        );
+    }
+
+    private static Stream<Arguments> invalidPageValueCases() {
+        return Stream.of(
+                Arguments.of("pageNo", successResponse(pageBody("one", "100", "0"))),
+                Arguments.of("numOfRows", successResponse(pageBody("1", "hundred", "0"))),
+                Arguments.of("totalCount", successResponse(pageBody("1", "100", "none")))
+        );
+    }
+
+    private static String successResponse(String body) {
+        return "<response><header><resultCode>000</resultCode><resultMsg>OK</resultMsg></header>"
+                + body
+                + "</response>";
+    }
+
+    private static String pageBody(String pageNo, String numOfRows, String totalCount) {
+        return "<body><items/>"
+                + pageElement("pageNo", pageNo)
+                + pageElement("numOfRows", numOfRows)
+                + pageElement("totalCount", totalCount)
+                + "</body>";
+    }
+
+    private static String pageElement(String tagName, String value) {
+        return value == null ? "" : "<" + tagName + ">" + value + "</" + tagName + ">";
     }
 }
