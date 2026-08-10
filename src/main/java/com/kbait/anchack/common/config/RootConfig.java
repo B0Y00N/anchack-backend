d63724be264539eb9466ce438b8c9a873f8dbcb5
jewook <wpdnrdl1026@gmail.com>
2026-08-10 17:15:22 +0900

    feat: 카카오 장소 API 클라이언트 추가

--- src/main/java/com/kbait/anchack/common/config/RootConfig.java
+++ src/main/java/com/kbait/anchack/common/config/RootConfig.java
@@ -1,6 +1,7 @@
 package com.kbait.anchack.common.config;
 
 import com.kbait.anchack.ingestion.config.MolitRentConfig;
+import com.kbait.anchack.place.config.PlaceConfig;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -29,7 +30,10 @@ import javax.sql.DataSource;
         "com.kbait.anchack.*.service",
         "com.kbait.anchack.common.security"
 })
-@Import(MolitRentConfig.class)
+@Import({
+    MolitRentConfig.class,
+    PlaceConfig.class
+})
 @EnableTransactionManagement
 public class RootConfig {
 
---
+++ src/main/java/com/kbait/anchack/place/config/PlaceConfig.java
@@ -0,0 +1,71 @@
+package com.kbait.anchack.place.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kbait.anchack.place.client.KakaoPlaceApiClient;
+import com.kbait.anchack.place.parser.KakaoPlaceParser;
+import com.kbait.anchack.place.validator.KakaoPlaceCategoryValidator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class PlaceConfig {
+
+    @Bean
+    public KakaoPlaceApiProperties kakaoPlaceApiProperties(
+            @Value("${kakao.place.rest-api-key:${KAKAO_PLACE_REST_API_KEY:}}") String restApiKey,
+            @Value("${kakao.place.radius:${KAKAO_PLACE_RADIUS:7000}}") int radius,
+            @Value("${kakao.place.page-size:${KAKAO_PLACE_PAGE_SIZE:15}}") int pageSize,
+            @Value("${kakao.place.max-page:${KAKAO_PLACE_MAX_PAGE:45}}") int maxPage,
+            @Value("${kakao.place.connect-timeout-ms:${KAKAO_PLACE_CONNECT_TIMEOUT_MS:5000}}") int connectTimeoutMs,
+            @Value("${kakao.place.read-timeout-ms:${KAKAO_PLACE_READ_TIMEOUT_MS:30000}}") int readTimeoutMs
+    ) {
+        return new KakaoPlaceApiProperties(
+                restApiKey,
+                radius,
+                pageSize,
+                maxPage,
+                connectTimeoutMs,
+                readTimeoutMs
+        );
+    }
+
+    @Bean(name = "kakaoPlaceRestTemplate")
+    public RestTemplate kakaoPlaceRestTemplate(KakaoPlaceApiProperties properties) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(properties.getConnectTimeoutMs());
+        requestFactory.setReadTimeout(properties.getReadTimeoutMs());
+
+        return new RestTemplate(requestFactory);
+    }
+
+    @Bean
+    public KakaoPlaceCategoryValidator kakaoPlaceCategoryValidator() {
+        return new KakaoPlaceCategoryValidator();
+    }
+
+    @Bean(name = "placeObjectMapper")
+    public ObjectMapper placeObjectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public KakaoPlaceParser kakaoPlaceParser(
+            @Qualifier("placeObjectMapper") ObjectMapper objectMapper,
+            KakaoPlaceCategoryValidator categoryValidator
+    ) {
+        return new KakaoPlaceParser(objectMapper, categoryValidator);
+    }
+
+    @Bean
+    public KakaoPlaceApiClient kakaoPlaceApiClient(
+            @Qualifier("kakaoPlaceRestTemplate") RestTemplate restTemplate,
+            KakaoPlaceApiProperties properties,
+            KakaoPlaceParser parser
+    ) {
+        return new KakaoPlaceApiClient(restTemplate, properties, parser);
+    }
+}
